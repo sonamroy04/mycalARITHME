887340cd5a904a9141b6c79ad1f7261dcbb280d6
Sonam <78811627+sonamroy04@users.noreply.github.com>
2022-07-21 12:31:00 +0530

    Add files via upload
    upload

---
+++ calu12Proj.java
@@ -0,0 +1,51 @@
+package myPRO2;
+import java.util.Scanner;
+
+public class calu12Proj {
+
+                public static void main(String[] args) {
+				char operator;
+				Double num1,num2;
+				Double result;
+				Scanner input = new Scanner(System.in);
+				
+				System.out.println("Enter the operator: +,-,*,/");
+				operator = input.next().charAt(0);
+				System.out.println("Enter the first number");
+				
+				num1 = input.nextDouble();
+				System.out.println("Enter the second number");
+				
+				num2 = input.nextDouble();
+				
+				switch(operator) {
+				case'*':
+					result = num1*num2;
+					System.out.println(num1 +"*" + num2 +"="+result);
+					break;
+				case'/':
+					result = num1/num2;
+					System.out.println(num1 +"/" + num2 +"="+result);
+					break;
+				case'+':
+					result = num1+num2;
+					System.out.println(num1 +"+" + num2 +"="+result);
+					break;
+				case'-':
+					result = num1-num2;
+					System.out.println(num1 +"-" + num2 +"="+result);
+					break;
+					
+					default:
+						System.out.println("invalid operator");
+						break;
+					
+					
+				}
+				input.close();
+				
+		}
+
+		}
+
+
